530b2498a76da9de705972b21f72497a7e8a8a71
Ignacy Buczek <ignacy.buczek@onet.pl>
2025-12-22 18:11:24 +0100

    Add a function for enforcing regularity assumptions

---
+++ problems/assumption-utils.hpp
@@ -0,0 +1,13 @@
+#include "flag-calculator.hpp"
+
+template <int theta, int vertices>
+FlagVector<0, 2 * vertices> regularity_assumption(flag blueprint) {
+  flag type;
+  blueprint.get_type_subflag(type);
+  FlagVector<theta, vertices> flag_vector(blueprint);
+  FlagVector<0, theta> root_vector(type);
+  auto unlabeled = flag_vector.template project<0, vertices>();
+  auto squared = flag_vector * flag_vector;
+  return unlabeled * unlabeled -
+         squared.template project<0, 2 * vertices - theta>() * root_vector;
+}
