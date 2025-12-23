dacf8b2d573215eb3b815254f88d1f9c59d593b8
Ignacy Buczek <ignacy.buczek@onet.pl>
2025-12-23 15:48:49 +0100

    Fix a bug in the regularity assumption function

--- problems/assumption-utils.hpp
+++ problems/assumption-utils.hpp
@@ -2,8 +2,10 @@
 
 template <int theta, int vertices>
 FlagVector<0, 2 * vertices> regularity_assumption(flag blueprint) {
+  cerr << "Regularity assumption " << blueprint.print() << endl;
   flag type;
   blueprint.get_type_subflag(type);
+  type.remove_labeled_vertices();
   FlagVector<theta, vertices> flag_vector(blueprint);
   FlagVector<0, theta> root_vector(type);
   auto unlabeled = flag_vector.template project<0, vertices>();
