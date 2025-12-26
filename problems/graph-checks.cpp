50a39da0a17293a869e35566ab902968e5c45565
Ignacy Buczek <ignacy.buczek@onet.pl>
2025-12-26 20:53:52 +0100

    Add a dummy program for checking flags

---
+++ problems/graph-checks.cpp
@@ -0,0 +1,15 @@
+#include "config.hpp"
+#include "flag-calculator.hpp"
+#include "option-parser.hpp"
+#include <cassert>
+#include <string>
+
+double BOUND = 0.0662;
+
+int main(int argc, char *argv[]) {
+  ProblemConfig::instance().data_directory = "graph-checks";
+  ProblemConfig::instance().upper_bound = false;
+  parse_options(argc, argv, ProblemConfig::instance());
+  FlagVector<0, V> flags;
+  flags *= 2;
+}
