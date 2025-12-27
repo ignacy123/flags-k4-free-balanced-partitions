38de464862598db3b41e4cb516c4a60fa8a3136b
Ignacy Buczek <ignacy.buczek@onet.pl>
2025-12-27 10:47:15 +0100

    Move forbidden files graph checks to a separate directory

--- problems/graph-checks.cpp
+++ problems/graph-checks.cpp
@@ -8,6 +8,7 @@ double BOUND = 0.0662;
 
 int main(int argc, char *argv[]) {
   ProblemConfig::instance().data_directory = "graph-checks";
+  ProblemConfig::instance().data_directory = "graph-checks-forbidden";
   ProblemConfig::instance().upper_bound = false;
   parse_options(argc, argv, ProblemConfig::instance());
   FlagVector<0, V> flags;
